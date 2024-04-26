21f068cc9befe8ddc5410155a90153a0e0341230
Twenty-Seven Pockets <tws-pocket@mail.de>
2024-04-26 12:44:04 +0200

    adding dist subtree

---
+++ js/page-news.bdb03832.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkui"]=self["webpackChunkui"]||[]).push([[814],{1183:function(n,u,e){e.r(u),e.d(u,{default:function(){return s}});var r=e(6768);function i(n,u,e,i,t,a){return(0,r.uX)(),(0,r.CE)("div",null," Ankündigung 1 ")}var t={name:"NewsView"},a=e(1241);const c=(0,a.A)(t,[["render",i]]);var s=c}}]);
+//# sourceMappingURL=page-news.bdb03832.js.map
